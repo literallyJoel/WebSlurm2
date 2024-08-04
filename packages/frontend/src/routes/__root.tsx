548725bb950d875219fc46b0f885fced2e1ae9a9
Joel Vivian <joel@jdvivian.co.uk>
2024-08-04 22:21:00 +0100

    added profile image to toolbar

--- packages/frontend/src/routes/__root.tsx
+++ packages/frontend/src/routes/__root.tsx
@@ -3,7 +3,8 @@ import { queryClient } from "@/main";
 import { isSetupComplete } from "@/api/setup";
 import isAuthenticated from "@/helpers/isAuthenticated";
 import { Link } from "@tanstack/react-router";
-
+import appStore from "@/stores/appStore";
+import defaultAvatar from "@/assets/image/profile/default.jpg";
 const isSetupCompleted = async () => {
   const result = await queryClient.fetchQuery({
     queryKey: ["isSetup"],
@@ -33,13 +34,21 @@ export const Route = createRootRoute({
   component: () => (
     <main className="flex min-h-screen flex-col items-center bg-slate-900 text-white">
       {!location.pathname.includes("/setup") && (
-        <div className="w-full flex flex-row p-4">
+        <div className="w-full flex flex-row py-4 px-12 justify-between">
           <Link
             className="text-2xl font-bold"
             to={location.pathname.includes("/auth") ? "" : "/"}
           >
             Web<span className="text-fuchsia-600">Slurm</span>
           </Link>
+
+          {appStore.state.tokenData && (
+            <img
+              src={appStore.state.tokenData.image ?? defaultAvatar}
+              alt="avatar"
+              className="w-10 h-10 rounded-full"
+            />
+          )}
         </div>
       )}
       <Outlet />
